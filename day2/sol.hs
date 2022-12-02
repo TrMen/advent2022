22818d87c4bfbc5eb89a9e6e9535d1057460c0f6
Tristan <tr.menke@web.de>
2022-12-02 06:38:00 +0100

    day2

---
+++ day2/sol.hs
@@ -0,0 +1,23 @@
+(part1, part2) = (sum . fmap p1 <$> inp, sum . fmap p2 <$> inp)
+  where
+    inp = fmap words . lines <$> readFile "day1/input.txt"
+    p1 xs = case (head xs, xs !! 1) of
+      ("A", "X") -> 4 -- Rock, Rock
+      ("B", "Y") -> 5 -- Paper, Paper
+      ("C", "Z") -> 6 -- Scissors, Scissors
+      ("B", "X") -> 1 -- Paper, Rock
+      ("A", "Y") -> 8 -- Rock, Paper
+      ("A", "Z") -> 3 -- Rock, Scissors
+      ("B", "Z") -> 9 -- Paper, Scissors
+      ("C", "X") -> 7 -- Scissors, Rock
+      ("C", "Y") -> 2 -- Scissors, Paper
+    p2 xs = case (head xs, xs !! 1) of
+      ("A", "X") -> 3 -- Rock, Lose -> Scissors
+      ("B", "X") -> 1 -- Paper, Lose -> Rock
+      ("C", "X") -> 2 -- Scissors, Lose -> Paper
+      ("A", "Y") -> 4 -- Rock, Tie
+      ("B", "Y") -> 5 -- Paper, Tie
+      ("C", "Y") -> 6 -- Scissors, Tie
+      ("A", "Z") -> 8 -- Rock, Win  -> Paper
+      ("B", "Z") -> 9 -- Paper, Win -> Scissors
+      ("C", "Z") -> 7 -- Scissors, Win -> Rock
