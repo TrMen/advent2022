3053d8a8d21987de826fd0987aa4312781eee804
Rohit Rao <rohit@rohitsrao.com>
2022-12-02 04:47:12 +0100

    Solution Day

---
+++ day1/sol.py
@@ -0,0 +1,27 @@
+with open('input.txt', 'r') as file:
+    calories = file.read().rstrip()
+
+calories = calories.split('\n')
+calories = [0 if val == '' else val for val in calories]
+calories = [int(val) for val in calories]
+
+elf_list = []
+elf_sub_list = []
+for val in calories:
+    if val != 0:
+        elf_sub_list.append(val)
+    else:
+        elf_list.append(elf_sub_list)
+        elf_sub_list = []
+
+elf_list = [sum(elf_sub_list) for elf_sub_list in elf_list]
+
+max_cal = max(elf_list)
+max_index = elf_list.index(max(elf_list))
+
+print(f'Max calories is {max_cal} and is carried by Elf {max_index + 1}')
+
+elf_list.sort(reverse=True)
+print(elf_list[0:3])
+first_three = sum(elf_list[0:3])
+print(f'Sum of top 3 elf calories - {first_three}')
