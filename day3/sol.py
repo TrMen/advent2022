2dfcdc82b43241ceb345ef50b7b29f32301e3e0e
Rohit Rao <rohit@rohitsrao.com>
2022-12-03 14:25:39 +0100

    Solution for day3

--- day3/sol.py
+++ day3/sol.py
@@ -3,14 +3,27 @@ with open('input.txt', 'r') as file:
 
 rucksacks = [rucksack.rstrip() for rucksack in rucksacks]
 
+
+def get_priority(char):
+    if (char.islower()):
+        return ord(char) - ord('a') + 1
+    elif (char.isupper()):
+        return ord(char) - ord('A') + 27
+
+
 priority = 0
 for rucksack in rucksacks:
     firsthalf = rucksack[:len(rucksack)//2]
     secondhalf = rucksack[len(rucksack)//2:]
     common_char = ''.join(set(firsthalf).intersection(secondhalf))
-    if (common_char.islower()):
-        priority += ord(common_char) - ord('a') + 1
-    elif (common_char.isupper()):
-        priority += ord(common_char) - ord('A') + 27
+    priority += get_priority(common_char)
 
-print(f'Total priority - {priority}')
+print(f'Total priority for part 1 - {priority}')
+
+priority2 = 0
+for i in range(0, len(rucksacks), 3):
+    set_list = [set(rucksack) for rucksack in rucksacks[i:i+3]]
+    common_char = ''.join(set.intersection(*set_list))
+    priority2 += get_priority(common_char)
+
+print(f'Total priority for part 2 - {priority2}')
